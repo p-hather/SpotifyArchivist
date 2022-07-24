65fd9ea442dcb56082111c1ad0976f6c2afffffe
Phil Hather <philhather@gmail.com>
2022-07-24 21:36:49 +0100

    remove comments, dev import

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from services.bigquery import bigquery
 from dotenv import load_dotenv
 from os import path
 import json
-import pyperclip
 
 load_dotenv()
 
@@ -13,25 +12,17 @@ def generate_schema(fp, example_record):
         print(f'Schema document already exists at {fp}')
     else:
         schema = bigquery.get_bq_schema(example_record)
-        with open(path, 'w') as file:
+        with open(fp, 'w') as file:
             json.dump(schema, file, indent=4)
             print(f'Schema document created at {fp}')
 
 def extract_load_listening_history(sp_client, bq_client):
-    # listening_history = sp_client.get_history()
-    # print(f'length of listening_history is {len(listening_history)}')
-    # listening_history_json = [json.dumps(record) for record in listening_history]
-    # print(f'length of listening_history_json is {len(listening_history_json)}')
-    # with open('lh.json', 'w') as file:
-    #     json.dump(listening_history, file, indent=4)
-    # with open('lhj.json', 'w') as file:
-    #     json.dump(listening_history_json, file, indent=4)
-    #bq_client.insert_rows(listening_history_json)
-    #bq_client.insert_rows(pyperclip.paste())
-
-    with open('lh.json') as file:
-        lh = json.load(file)
-    bq_client.insert_rows(lh)
+    listening_history = sp_client.get_history()
+    if not listening_history:
+        print('No listening history found - exiting function')
+        return
+    print(f'{len(listening_history)} tracks returned')
+    bq_client.insert_rows(listening_history)
 
 def main():
     sp = spotifyExtract()
@@ -40,8 +31,8 @@ def main():
     dataset = 'spotify_archivist'
     table = 'listening_history'
     schema_fp = './services/bigquery/schema/listening_history.json'
-    bq = bigquery.bigQueryLoad(project, dataset, table, schema_fp)
-
+    bq = bigquery.bigQueryLoad(project, dataset, table)
+    bq.create_table(schema_fp)
     extract_load_listening_history(sp, bq)
 
 
