1d74da83fae036f97440dfd26b4ebc235a44ba51
Phil Hather <philhather@gmail.com>
2022-08-12 18:45:08 +0100

    work around connection timeout issue

--- services/spotify.py
+++ services/spotify.py
@@ -4,16 +4,19 @@ import time
 from datetime import datetime
 import shelve
 import logging
+from requests.exceptions import ReadTimeout
 
 
 class spotifyExtract:
     def __init__(self):
         self.client = self.spotify_auth()
-        
+
     def spotify_auth(self):
         scopes = 'user-read-recently-played'
         logging.info(f'Attempting to authenticate Spotify access with scopes: {scopes}')
-        return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scopes, open_browser=False))
+        return spotipy.Spotify(
+            auth_manager=SpotifyOAuth(scope=scopes, open_browser=False),
+            requests_timeout=10, retries=10)
     
     def unix_ms_strf(self, unix_ms):
         ts = datetime.utcfromtimestamp(int(unix_ms)/1000)
@@ -32,7 +35,13 @@ class spotifyExtract:
             else:
                 logging.info(f'Getting recently played tracks after {self.unix_ms_strf(after)}')
             
-            recently_played = self.client.current_user_recently_played(after=after)
+            # Work around connection timeout issue by re-instantiating Spotify client
+            try:
+                recently_played = self.client.current_user_recently_played(after=after)
+            except ReadTimeout:
+                self.client = self.spotify_auth()
+                recently_played = self.client.current_user_recently_played(after=after)
+
             tracks = recently_played['items']
 
             # This endpoint will only return the last 50 results, so the pagination below is not 
